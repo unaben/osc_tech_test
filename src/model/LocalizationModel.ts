929952c37b8b61b684bbee53ae4ba4f1655539b6
unaben <unaben@yahoo.com>
2023-05-10 00:30:21 +0100

    [master] add translate & fix style

---
+++ src/model/LocalizationModel.ts
@@ -0,0 +1,23 @@
+export interface IMessage {
+  orderSummary: string;
+  completeCheckout: string;
+  tax: string;
+  shipping: string;
+  totalPrice: string;
+  grandTotal: string;
+  product: string;
+  id: string;
+  price: string;
+  clearCart: string;
+  continueShopping: string;
+  yourBag: string;
+  shoppingBasket: string;
+  yourWishList: string;
+  offer: string;
+  store: string;
+  cart: string;
+  title: string;
+  checkoutMessage: string;
+  addToCart: string;
+  NoItemInCart: string;
+}
---
+++ src/translate/ENT.ts
@@ -0,0 +1,25 @@
+import { IMessage } from "../model/LocalizationModel";
+
+export const message = {
+  orderSummary: "order summary",
+  completeCheckout: "complete checkout",
+  tax: "Tax",
+  shipping: "Shipping",
+  totalPrice: "Total Price",
+  grandTotal: "Grand Total",
+  product: "Product",
+  id: "ID",
+  price: "Price",
+  clearCart: "clear cart",
+  continueShopping: "continue shopping",
+  yourBag: "your bag",
+  shoppingBasket: "Shopping bag",
+  yourWishList: "Your wish list",
+  offer: "Super offer! free shipping on order over £50",
+  store: "store",
+  cart: "cart",
+  title: "ColletionX",
+  checkoutMessage: "Thanks for shopping with us today",
+  addToCart: "add to cart",
+  NoItemInCart: 'No item in cart'
+} as IMessage;
